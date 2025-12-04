93fc1b66a2bf4c1b3c03371d8b72ee2185b1e516
uwizerah <u.kabasinga@gmail.com>
2025-12-04 16:58:59 +0200

    fix: make sure tailwindcss is working

--- frontend/sec-certificate-ui/src/app/features/templates/template-list.component.ts
+++ frontend/sec-certificate-ui/src/app/features/templates/template-list.component.ts
@@ -2,19 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ApiService, Template } from '../../core/api/api.service';
-import { TemplateEditorComponent } from './template-editor.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, TemplateEditorComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './template-list.component.html'
 })
 export class TemplateListComponent implements OnInit {
+
   templates: Template[] = [];
   loading = true;
-  showModal = false;
 
   constructor(private api: ApiService) {}
+
   ngOnInit(): void {
     this.load();
   }
@@ -25,20 +25,5 @@ export class TemplateListComponent implements OnInit {
       this.templates = res;
       this.loading = false;
     });
-  }
-
-  reload() {
-    this.loading = true;
-    this.api.getTemplates().subscribe(res => {
-      this.templates = res;
-      this.loading = false;
-    });
-  }
-
-  closeModal(refresh: boolean = false) {
-    this.showModal = false;
-    if (refresh) {
-      this.load();
-    }
   }
 }
